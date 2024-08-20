610fdc5f9d07e6e8490e0f0235d13ac11ddcc192
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-08-20 17:41:13 +0200

    update routage

---
+++ src/pages/profile/index.ts
@@ -0,0 +1,22 @@
+import { render , html, ViewTemplate , ViewContext } from '@lithium-framework/core';
+import '@lithium-framework/router-element'
+
+
+let template: ViewTemplate<any> = html`${( context:ViewContext )=>{
+
+
+  return html`<div>
+        <h1>Profile</h1>
+        <nav>
+            <a href='compte'>Compte</a>
+            <a href='inscriptions'>S'inscrire aux cours</a>
+            <a href='informations'>Informations supplémentaires</a>
+            <a href='/'>Déconnexion</a>
+        </nav>
+    </div>`;
+
+}}`
+
+
+
+render(template);
