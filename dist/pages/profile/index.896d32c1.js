14c38a21617215da3da22ac5358edbcb7691f5c8
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-10-15 11:28:50 +0200

    update

--- dist/pages/profile/index.896d32c1.js
+++ dist/pages/profile/index.896d32c1.js
@@ -621,7 +621,7 @@ let template = (0, _core.html)`${(context)=>{
     </div>
     <div>
       <pf-panel header scrollable>
-        <h1 slot = "header" >MyContent</h1>
+        <h1 slot = "header" >Profile</h1>
         <div>
           <span>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium est a porttitor vehicula. Quisque vel commodo urna. Morbi mattis rutrum ante, id vehiculex accumsan ut. Morbi viverra, eros vel porttitor facilisis, eros purus aliquet erat, nec lobortis felis elit pulvinar sem. Vivamus vulputate, risus eget commodeleifend, eros nibh porta quam, vitae lacinia leo libero at magna. Maecenas aliquam sagittis orci, et posuere nisi ultrices sit amet. Aliquam ex odio, malesuada seposuere quis, pellentesque at mauris. Phasellus venenatis massa ex, eget pulvinar libero auctor pretium. Aliquam erat volutpat. Duis euismod justo in quaullamcorper, in commodo massa vulputate.
---
+++ server/public/pages/inscriptions/index.05dafa42.js
@@ -0,0 +1,27 @@
+var e=globalThis,i={},l={},t=e.parcelRequirec605;null==t&&((t=function(e){if(e in i)return i[e].exports;if(e in l){var t=l[e];delete l[e];var r={id:e,exports:{}};return i[e]=r,t.call(r.exports,r,r.exports),r.exports}var a=Error("Cannot find module '"+e+"'");throw a.code="MODULE_NOT_FOUND",a}).register=function(e,i){l[e]=i},e.parcelRequirec605=t),t.register;var r=t("1BF7I");t("lTDtW"),t("5OQrz");let a=(0,r.html)`${e=>(0,r.html)`<div class='Sign-in'>
+          <h1>Inscription</h1>
+          <form class='Sign-form'>
+              <div class='input-fields'>
+                  <label for="email">E-mail</label>
+                  <input type="email">
+              </div>
+              <div class='input-fields'>
+                  <label for="password">Mot de passe</label>
+                  <input type="password">
+              </div>
+              <div class='input-fields'>
+                  <label for="Date_naissance">Date de naissance</label>
+                  <input type="date">
+              </div>
+              <div class='input-fields'>
+                  <label for="prénom">Prénom</label>
+                  <input type="text">
+              </div>µ
+              <div class='input-fields'>
+                  <label for="nom">Nom</label>
+                  <input type="text">
+              </div>
+              <button type="submit">Inscription</button>
+          </form>
+      </div>`}`;(0,r.render)(a);
+//# sourceMappingURL=index.05dafa42.js.map
