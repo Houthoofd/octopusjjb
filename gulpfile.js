6358aac0b070293f6cce487bb323f75c046204a7
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-02 11:18:08 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,38 @@ const fs = require('fs');
 
 gulp.task('clean-old-bundles', async function () {
   const { deleteAsync } = await import('del');
-  const bundleDir = path.join('server', 'public');
-  
-  // Récupérer tous les fichiers dans le dossier de bundle, sauf ceux dans 'images'
-  const files = fs.readdirSync(bundleDir)
-    .map(file => ({
-      name: file,
-      path: path.join(bundleDir, file),
-      time: fs.statSync(path.join(bundleDir, file)).mtime.getTime() // Obtenir la date de modification
-    }))
-    .filter(file => !file.path.includes(path.join(bundleDir, 'images'))) // Exclure le dossier 'images'
-    .sort((a, b) => b.time - a.time); // Trier par date de modification (plus récent en premier)
 
-  // Garder uniquement le fichier le plus récent
-  const latestFile = files[0]?.name;
+  // Dossiers à nettoyer
+  const directories = [
+    path.join('server', 'public'),
+    path.join('dist')
+  ];
 
-  // Supprimer tous les fichiers sauf le plus récent
-  if (latestFile) {
-    const filesToDelete = files.slice(1).map(file => file.path);
-    await deleteAsync(filesToDelete);
-    console.log('Ancien(s) bundle(s) supprimé(s) :', filesToDelete);
-  } else {
-    console.log('Aucun fichier à supprimer.');
+  for (const dir of directories) {
+    // Récupérer tous les fichiers dans le dossier
+    const files = fs.readdirSync(dir)
+      .map(file => ({
+        name: file,
+        time: fs.statSync(path.join(dir, file)).mtime.getTime() // Obtenir la date de modification
+      }))
+      .sort((a, b) => b.time - a.time); // Trier par date de modification (plus récent en premier)
+
+    // Garder uniquement le fichier le plus récent
+    const latestFile = files[0]?.name;
+
+    // Supprimer tous les fichiers sauf le plus récent, en excluant le dossier 'images'
+    if (latestFile) {
+      const filesToDelete = files
+        .slice(1) // Exclure le fichier le plus récent
+        .map(file => path.join(dir, file.name))
+        .filter(file => !file.includes(path.join('server', 'public', 'images'))); // Exclure les fichiers dans 'images'
+
+      if (filesToDelete.length > 0) {
+        await deleteAsync(filesToDelete);
+        console.log(`Ancien(s) fichier(s) supprimé(s) dans ${dir}:`, filesToDelete);
+      }
+    } else {
+      console.log(`Aucun fichier à supprimer dans ${dir}.`);
+    }
   }
 });
-
-
