6b68018a38469f7496766b4c537576298b4aa05a
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-03 19:13:57 +0100

    update

--- server/src/routes/connexion.ts
+++ server/src/routes/connexion.ts
@@ -52,7 +52,6 @@ router.post('/', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,         // Le cookie ne peut être accédé que par le serveur, pas par JavaScript
-      secure: false,          // Ne pas forcer HTTPS si vous utilisez HTTP
       sameSite: 'none',       // Permet l'envoi du cookie dans les requêtes inter-domaines
       maxAge: 3600000,        // Durée de vie du cookie (1 heure)
       domain: '.octopusjjb.ovh', // Assurez-vous que le domaine est bien configuré
