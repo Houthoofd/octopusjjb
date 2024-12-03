b6a560bf9d57506e365e4f12786b7838782749af
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-03 12:34:22 +0100

    update

--- server/src/packages/db/mysqlconnector.ts
+++ server/src/packages/db/mysqlconnector.ts
@@ -6,11 +6,11 @@ export default class MysqlConnector {
   constructor() {
       // Configuration de la connexion à la base de données MySQL
       this.connection = mysql.createConnection({
-        host: 'mydbinstance.cxj7l8fskg9v.us-east-1.rds.amazonaws.com', // Remplacez par votre endpoint RDS
+        host: 'dbserver.c54ksqmeed2b.eu-central-1.rds.amazonaws.com', // Remplacez par votre endpoint RDS
         port: 3306, // Le port par défaut pour MySQL
         user: 'admin', // Remplacez par votre nom d'utilisateur RDS
         password: 'PtW143kjkS3F', // Remplacez par votre mot de passe RDS
-        database: 'octopusjjb',
+        database: 'dbserver',
       });
 
       // Établir la connexion à la base de données
