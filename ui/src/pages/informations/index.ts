670b11d8d022cdd46a539493047c43d2de46689c
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-04 16:34:19 +0100

    update

--- ui/src/pages/informations/index.ts
+++ ui/src/pages/informations/index.ts
@@ -122,7 +122,6 @@ export class Informations extends WebComponent {
         headers: {
           'Content-Type': 'application/json',
         },
-        credentials: 'include',
         body: JSON.stringify(userData),
       });
   
