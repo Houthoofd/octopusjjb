180c6b0c7d1d47e7d79a61d2d846717ddcbdf8ae
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-04 12:04:43 +0100

    update

--- ui/src/pages/cours/index.ts
+++ ui/src/pages/cours/index.ts
@@ -241,7 +241,9 @@ export class Cours extends WebComponent {
       // Vérifie si les données de l'utilisateur existent
       if (!userDataString) {
           // Si pas d'utilisateur connecté, redirige vers la page de connexion
-          window.location.href = '/pages/connexion';
+          setTimeout(() => {
+            window.location.href = '/pages/cours';
+          }, 1000);
           return; // Empêche l'exécution du reste du code
       }
   
