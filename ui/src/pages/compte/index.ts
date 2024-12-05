5d10bba5a598717fc7f186144e2f9207bc05922c
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-05 13:28:40 +0100

    update

--- ui/src/pages/compte/index.ts
+++ ui/src/pages/compte/index.ts
@@ -224,7 +224,7 @@ export class Compte extends WebComponent {
       const userData = JSON.parse(userDataString);
       console.log('Données utilisateur récupérées:', userData);
   
-      const response = await fetch(`${url}informations/`, {
+      const response = await fetch(`${url}informations`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
