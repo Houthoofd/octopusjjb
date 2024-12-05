bc0475642929bbaffcc5eb96a05c657315ef69e5
Benoit Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-05 14:07:05 +0100

    update

--- ui/src/pages/dashboard/index.ts
+++ ui/src/pages/dashboard/index.ts
@@ -471,11 +471,10 @@ export class Dashboard extends WebComponent {
             });
             console.log(updatedData)
             try {
-              const response = await fetch(`${url}infos/update/`, {
+              const response = await fetch(`${url}infos/update`, {
                   method: 'POST',
                   headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify({ user_id: user.id, data: updatedData}),
-                  credentials: 'include',
+                  body: JSON.stringify({ user_id: user.id, data: updatedData})
               });
       
               if (!response.ok) {
